218e86f18beafae9185a06c4f78b4b82ebd2e2f6
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-21 15:26:50 +0100

    adding ajax api call and linking postlist to posts

---
+++ src/components/Postlist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Postlist() {
+  return (
+    <div>Postlist</div>
+  )
+}
+
+export default Postlist
--- src/pages/ListofPunsPage.js
+++ src/pages/ListofPunsPage.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import Postlist from '../components/Postlist'
 
 function ListofPunsPage() {
+    //use hook useState to 
+    const [posts, setPosts] = useState([])
+
+
+    const fetchData = async () => {
+        try{
+            //obtain the data before we do anything else
+            const respone = await fetch("https://puns-app.herokuapp.com/puns")
+            const data = await respone.json() // need to parse the data secondly
+            console.log(data)
+
+            setPosts(data)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+    useEffect( () => {
+        fetchData();
+    }, []) //adding second argument for empty array so useEffect only runs once, at "ComponentDidMount"
   return (
-    <div>ListofPunsPage</div>
+    <div>
+        <h1>Posts</h1>
+
+        <Postlist />
+
+    </div>
   )
 }
 
