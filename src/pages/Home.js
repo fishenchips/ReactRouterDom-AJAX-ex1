37ffff6100df879ad16eb066a61170b6559b8121
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-21 14:29:28 +0100

    first commit, adding pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h3>Welcome to your favourite website. Let's have some fun!</h3>
+    </div>
+  )
+}
+
+export default Home
